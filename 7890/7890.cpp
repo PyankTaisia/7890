59b97dafa769c9849fa999125a1f5efd9b399652
ШС <@DESKTOP-JO2G247>
2022-05-12 20:56:13 +0300

    Добавьте файлы проекта.

---
+++ 7890/7890.cpp
@@ -0,0 +1,108 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	/*int sumpok;
+
+	cout << "введите сумму покупки: \n";
+	cin >> sumpok;
+
+	if (sumpok >= 1000)
+	{
+		cout << "скидка 10%";
+	}
+	else
+	{
+		cout << "скидки нет";
+	}*/
+
+
+
+	/*int n;
+	int a = rand() % 10;
+	int b = rand() % 10;
+	int c = rand() % 4;
+
+	if (c == 0)
+	{
+		cout << a << " + " << b;
+		cin >> n;
+		if (n == a + b)
+		{
+			cout << "верно";
+		}
+		else
+		{
+			cout << "вы ошиблись" << a + b;
+		}
+	}
+	else if (c == 1)
+	{
+		cout << a << "-" << b;
+		cin >> n;
+		if (n == a - b)
+		{
+			cout << "верно";
+		}
+		else
+		{
+			cout << "вы ошиблись" << a - b;
+		}
+	}
+	else if (c == 3)
+	{
+		cout << a << "*" << b;
+		cin >> n;
+		if (n == a * b)
+		{
+			cout << "верно";
+		}
+		else
+		{
+			cout << "вы ошиблись" << a * b;
+		}
+	}
+	else if (c == 4)
+	{
+		cout << a << "/" << b;
+		cin >> n;
+		if (n == a / b)
+		{
+			cout << "верно";
+		}
+		else
+		{
+			cout << "вы ошиблись" << a / b;
+		}
+	}*/
+
+
+	int userFoto,r;
+	cout << "сколько фотографий хотите распечатать?";
+	cin >> userFoto;
+	cout << "1 - 9*12\n";
+	cout << "2 - 10*15\n";
+	cout << "3 - 18*24\n";
+	cin >> r;
+
+	switch (r) {
+	case 1 :
+	{
+		cout << "";
+		break;
+	}
+	case 2:
+	{
+		break;
+	}
+	case 3:
+	{
+		break;
+	}
+
+
+}
+	
+
+ 
